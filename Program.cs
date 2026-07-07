645771ed91cf51ec273e5ccd9d6c04430a7d994e
virajdaxini2510 <daxini.viraj@gmail.com>
2026-07-07 13:12:21 +0530

    This is my second commit

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace DemoConsoleLib5EC1A
 {
+    /// <summary>
+    /// Class Program is the entry point of the console application. It demonstrates basic input/output operations, variable declarations, and conditional statements in C#.
+    /// </summary>
     internal class Program
     {
+        /// <summary>
+        /// Main method serves as the entry point of the application. It initializes variables, prompts the user for input, and displays messages based on the user's age.
+        /// </summary>
         static void Main()
         {
             int age;
@@ -18,10 +24,6 @@ namespace DemoConsoleLib5EC1A
             dept = "Computer Engineering";
 
             Console.WriteLine("\n Hello 5EC1A Batch Students");
-
-            //Console.WriteLine("Hello " + name);
-            //Console.WriteLine("Your age is " + age);
-            //Console.WriteLine("Your department is " + dept);
 
             Console.WriteLine("Hello " + name + " You are from " + dept + " Department " + "Your age is " + age);
 
